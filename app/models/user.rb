4641a107c6923f4e7ce1aee6c8090decdcf66f06
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-11-10 01:36:08 +0000

    Add frozen string literal comment to multiple files for performance optimization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
