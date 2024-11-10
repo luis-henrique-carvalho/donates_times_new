059b938edcddc76d00a981704b9813e2db778ec4
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-11-10 02:16:43 +0000

    Remove user-related test files and add articles table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_10_004155) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_10_015840) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
